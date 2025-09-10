bd0e833fca80371ef8be74426e2a1c2f294e313a
blakebezold <blakebezold@gmail.com>
2025-09-10 15:33:11 +0800

    Inital Commit

---
+++ get-review-urls.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import time
+import requests
+from bs4 import BeautifulSoup
+import sys
+
+def find_locs(url) -> list[str]:
+    sitemaps = []
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.content, 'xml')
+
+        for loc in soup.find_all('loc'):
+            sitemaps.append(loc.text)
+        return sitemaps
+    
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching URL: {e}")
+    return []
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print('Wrong number of arguments')
+        sys.exit(1)
+
+    starting_year = sys.argv[1]
+    ending_year = sys.argv[2]
+    
+    for year in range(2024, 2026):
+        for month in range(1, 13):
+            for week in range(1, 6):
+                url = f'https://pitchfork.com/sitemap.xml?year={year}&month={month}&week={week}'
+                print(f'!{year} {month} {week}')
+                for page in find_locs(url):
+                    if 'https://pitchfork.com/reviews/' in page:
+                        print(page)
+        time.sleep(0.3)
---
+++ get-reviews.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+import concurrent.futures
+import time
+import requests
+import json
+import sys
+
+def print_review(url):
+  res = requests.get(url)
+
+  # finding the album title
+  start_marker = r'<meta property="og:title" content="'
+  end_marker = r'"/>'
+
+  start_index = res.text.find(start_marker)
+
+  if start_index == -1:
+      return None
+  
+  start_index += len(start_marker)
+  end_index = res.text.find(end_marker, start_index)
+
+  if end_index == -1:
+      return None
+  
+  title = res.text[start_index:end_index + 1] 
+
+  # finding the description
+  start_marker = r'<meta property="og:description" content="'
+  end_marker = r'"/>'
+
+  start_index = res.text.find(start_marker)
+
+  if start_index == -1:
+      return None
+  
+  start_index += len(start_marker)
+  end_index = res.text.find(end_marker, start_index)
+
+  if end_index == -1:
+      return None
+  
+  description = res.text[start_index:end_index + 1].replace("\n", "")
+
+  # finding the rating 
+  start_marker = 'window.__PRELOADED_STATE__ = '
+  end_marker = '};'
+
+  start_index = res.text.find(start_marker)
+
+  if start_index == -1:
+      return None
+  
+  start_index += len(start_marker)
+  end_index = res.text.find(end_marker, start_index)
+
+  if end_index == -1:
+      return None
+  
+  json_text = res.text[start_index:end_index + 1] 
+
+  data = json.loads(json_text)
+  score = data.get('transformed', {}) \
+              .get('review', {}) \
+              .get('headerProps', {}) \
+              .get('musicRating', {}) \
+              .get('score')
+  
+  return f'{title}|{score}|{description}|{url}'
+  
+
+def read_urls():
+    with open('urls.txt') as file:
+      for line in file:
+          yield line.strip()
+    
+
+if __name__ == '__main__':
+  urls = read_urls()
+
+  start_time = time.time()
+
+  with concurrent.futures.ThreadPoolExecutor() as executor:
+      futures = [executor.submit(print_review, url) for url in urls]
+      for future in concurrent.futures.as_completed(futures):
+          print(future.result())
+
+  end_time = time.time()
+  print(f"Requested reviews in {end_time - start_time:.2f} seconds")
